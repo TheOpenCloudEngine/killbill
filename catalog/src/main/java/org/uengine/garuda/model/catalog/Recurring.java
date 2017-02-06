82bc45b161d272abdb8118e3cc4717b79a84f2f4
SeungpilPark <sppark@uengine.org>
2017-02-06 15:52:07 +0900

    #21 Mybatis sql factory and services, mappers, models

---
+++ catalog/src/main/java/org/uengine/garuda/model/catalog/Recurring.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014-2017 Groupon, Inc
+ * Copyright 2014-2017 The Billing Project, LLC
+ *
+ * The Billing Project licenses this file to you under the Apache License, version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License.  You may obtain a copy of the License at:
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.uengine.garuda.model.catalog;
+
+import java.util.List;
+
+/**
+ * Created by uengine on 2017. 2. 3..
+ */
+public class Recurring {
+    private String billingPeriod;
+    private List<Price> recurringPrice;
+
+    public String getBillingPeriod() {
+        return billingPeriod;
+    }
+
+    public void setBillingPeriod(String billingPeriod) {
+        this.billingPeriod = billingPeriod;
+    }
+
+    public List<Price> getRecurringPrice() {
+        return recurringPrice;
+    }
+
+    public void setRecurringPrice(List<Price> recurringPrice) {
+        this.recurringPrice = recurringPrice;
+    }
+}
